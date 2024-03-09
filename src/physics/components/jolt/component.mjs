71e7a832e2f803729aa571f87fcc66faed2cc832
LeXXik <alexander.emeliyanov@gmail.com>
2024-03-09 15:56:53 +0200

    fix

--- src/physics/components/jolt/component.mjs
+++ src/physics/components/jolt/component.mjs
@@ -230,44 +230,25 @@ class ShapeComponent extends pc.EventHandler {
         if (!isCompoundChild) {
             cb.write(props.density, BUFFER_WRITE_FLOAT32, false);
 
-            // const position = props.shapePosition;
-            // const rotation = props.shapeRotation;
-            // const massOffset = props.massOffset;
-            // const hasPositionOffset = !position.equals(pc.Vec3.ZERO);
-            // const hasRotationOffset = forceWriteRotation || !rotation.equals(pc.Quat.IDENTITY);
-            // const hasShapeOffset = hasPositionOffset || hasRotationOffset;
-            // const hasMassOffset = !massOffset.equals(pc.Vec3.ZERO);
+            const position = props.shapePosition;
+            const rotation = props.shapeRotation;
+            const massOffset = props.massOffset;
+            const hasPositionOffset = !position.equals(pc.Vec3.ZERO);
+            const hasRotationOffset = forceWriteRotation || !rotation.equals(pc.Quat.IDENTITY);
+            const hasShapeOffset = hasPositionOffset || hasRotationOffset;
+            const hasMassOffset = !massOffset.equals(pc.Vec3.ZERO);
     
-            // cb.write(hasShapeOffset, BUFFER_WRITE_BOOL, false);
-            // if (hasShapeOffset) {
-            //     cb.write(position, BUFFER_WRITE_VEC32, false);
-            //     cb.write(rotation, BUFFER_WRITE_VEC32, false);
-            // }
+            cb.write(hasShapeOffset, BUFFER_WRITE_BOOL, false);
+            if (hasShapeOffset) {
+                cb.write(position, BUFFER_WRITE_VEC32, false);
+                cb.write(rotation, BUFFER_WRITE_VEC32, false);
+            }
     
-            // cb.write(hasMassOffset, BUFFER_WRITE_BOOL, false);
-            // if (hasMassOffset) {
-            //     cb.write(massOffset, BUFFER_WRITE_VEC32, false);
-            // }
+            cb.write(hasMassOffset, BUFFER_WRITE_BOOL, false);
+            if (hasMassOffset) {
+                cb.write(massOffset, BUFFER_WRITE_VEC32, false);
+            }
         }
-
-        const position = props.shapePosition;
-        const rotation = props.shapeRotation;
-        const massOffset = props.massOffset;
-        const hasPositionOffset = !position.equals(pc.Vec3.ZERO);
-        const hasRotationOffset = forceWriteRotation || !rotation.equals(pc.Quat.IDENTITY);
-        const hasShapeOffset = hasPositionOffset || hasRotationOffset;
-        const hasMassOffset = !massOffset.equals(pc.Vec3.ZERO);
-
-        cb.write(hasShapeOffset, BUFFER_WRITE_BOOL, false);
-        if (hasShapeOffset) {
-            cb.write(position, BUFFER_WRITE_VEC32, false);
-            cb.write(rotation, BUFFER_WRITE_VEC32, false);
-        }
-
-        cb.write(hasMassOffset, BUFFER_WRITE_BOOL, false);
-        if (hasMassOffset) {
-            cb.write(massOffset, BUFFER_WRITE_VEC32, false);
-        }        
 
         return ok;
     }
